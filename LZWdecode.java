88487d5a45fa3019508baec0b4e82b24c87f5dd6
joe <grombus@mypc.com>
2024-04-16 19:15:59 +1200

    removed garbage code

--- LZWdecode.java
+++ LZWdecode.java
@@ -8,26 +8,21 @@ class LZWdecode {
     public static void main(String args[]) {
         Scanner reader = new Scanner(System.in);
         OutputStreamWriter writer = new OutputStreamWriter(System.out);
-        Trie trie = new Trie();
-        //LinkedList<String> phrases = new LinkedList<String>();
-        //to build up phrases of varying length
-        //ArrayList list = new ArrayList<Char>();
         LinkedList<Character> list = new LinkedList<Character>();
         HashMap<Integer, String> phrases = new HashMap<Integer, String>();
 
         String old = "";
         String neu = "";
-        int counter = 0;
         int phraseNum = 0;
+
+        //initialise hashmap
         for(int i = 0; i < 17; i++) {
             if(i < 10) {
                 phrases.put(i, String.valueOf(i));
-                //phrases.add(String.valueOf(i));
             } else {
                 char[] chars = new char[1];
                 chars[0] = (char)(i + 55);
                 phrases.put(i, new String(chars));
-                //phrases.add(new String(chars));
             }
             phraseNum++;
         }
@@ -36,80 +31,42 @@ class LZWdecode {
             while(reader.hasNext()) {
                 String next = reader.next();
                 int phrase = Integer.parseInt(next);
-                //System.out.println();
+    
                 //current phrase refers to the last phrase
                 if(phrase == phraseNum) {
-                    //System.out.println(phrase);
-                    //if(true) return;
-                    
-                    //System.out.println("here");
-                    //append first letter of old to itself
-                    //trie.insert(old, old.substring(0,1));
-                    
+
+                    //end of phrase is the same as the first character in it
                     old += old.substring(0,1);
                     phrases.put(phraseNum, old);
                     writer.write(old);
-                    //phrases.add(old);
+ 
                     phraseNum++;
                     
                 } else {
-                    
-                    //TrieNode node = trie.findPhrase(phrase);
 
                     TrieNode node = null;
                     char[] chars;
-                    //list.clear();
-                    //System.out.println("PHRASE: " + phrase);
+
                     while(node != null) {
                         
                         list.add(new Character(node.getSuffix()));
-                        //System.out.println(new Character(node.getSuffix()));
-                        //System.out.println(node);
                         node = node.getParent();   
                     }
-                    //chars = new char[list.size()];
-                    //System.out.println("Current Run: " + counter);
-                    //to reverse the order of the chars
-                    //need to change this because it takes up so much time to compute
-                    //for(int i = 0; i < chars.length; i++) {
-                    //    chars[i] = list.get((list.size() - 1) - i).charValue();
-                    //}
-
-                    //neu = new String(chars);
                     
                     neu = phrases.get(phrase);
                     writer.write(neu);
-                    //System.out.println("OLD: " + old);
-                    //System.out.println("NEU: " + neu);
-                    //first letter of neu becomes end of old
-                    //System.out.println("STORING: " + old + neu.substring(0,1));
-                    //System.out.println(phrase);
-                    //System.out.println("COUNTER: " + counter);
-                    //System.out.println("CURR PHRASE " + trie.getNextPhraseNumber());
-                    //System.out.println("INSERTING: " + old + neu);
+          
                     if(old != "") {
-                        //trie.insert(old, neu.substring(0,1));
-                        //phrases.add(old += neu.substring(0,1));
                         phrases.put(phraseNum, old += neu.substring(0,1));
                         phraseNum++;
                     }
                     old = neu;
 
                 }
-                counter++;
-                //writer.flush();
-                
             }
             writer.close();
         } catch(IOException ex) {
-        System.err.println("IOException: " + ex);
+            System.err.println("IOException: " + ex);
         }
     }
 }
-
-
-
-//add sequence from given number to new variable
-//concatenate the first char onto the end of old variable
-//this is the new phrase, add it to the trie
-//old variable becomes new variable, as that's where the next sequence starts
